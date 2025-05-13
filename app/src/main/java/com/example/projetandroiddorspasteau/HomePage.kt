1d225fc85195479306381240d0ea1ec1fda05875
Sebastien Dors <dorssebastien@gmail.com>
2025-05-13 22:02:25 +0200

    Mise en place de la page d'accueil

---
+++ app/src/main/java/com/example/projetandroiddorspasteau/HomePage.kt
@@ -0,0 +1,15 @@
+package com.example.projetandroiddorspasteau
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page)
+
+    }
+}
